e782ad620d29f9012a8ce85f3b8f6d8a22cae877
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-09 16:31:58 -0500

    adding task up to 2

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 0. Coroutine called async_generator """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Coroutine that loops 10 times, each
+    time asynchronously wait 1 second """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
