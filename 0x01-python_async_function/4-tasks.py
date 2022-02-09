739d882bdd7cfd8a638cebd13baf24a67f53856b
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-09 16:28:52 -0500

    debugging

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-""" 4. Tasks"""
+""" 4. Asynchronous coroutine task_wait_n"""
 import asyncio
 from typing import List
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """4. Tasks"""
-    delay_list = []
-    for index in range(n):
-        delay_list.append(await task_wait_random(max_delay))
-    done = await asyncio.gather(*asyncio.as_completed(delay_list))
-    return sorted(done)
+    """ Function taht executes wait_random n times"""
+    array_delay = []
+    for i in range(n):
+        delay = task_wait_random(max_delay)
+        array_delay.append(await delay)
+
+    new_array = []
+
+    while array_delay:
+        min = array_delay[0]
+        for num in array_delay:
+            if num < min:
+                min = num
+        new_array.append(min)
+        array_delay.remove(min)
+
+    return new_array
