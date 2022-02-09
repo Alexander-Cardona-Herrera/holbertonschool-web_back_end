ef768de4153a5a5bffab1813ef07560977dde3ff
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-09 15:49:00 -0500

    adding task up to task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+'''3. Basic annotations - to string '''
+
+
+def to_str(n: float) -> str:
+    '''takes a float n as argument and returns
+    the string representation of the float.'''
+    return n.__str__()
