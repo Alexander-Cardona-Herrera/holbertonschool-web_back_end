4ccb58e27d5b3cb98567ebd6d010b2ed422b95a2
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-10 13:17:32 -0500

    adding advance tasks

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
-""" MRUCache module
-"""
-from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
+""" MRU Caching """
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ Class MRUCache """
-
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
-        """Initialiaze the class"""
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.head, self.tail = 'head', 'tail'
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        """ MRU algorithm, handle elements """
+        self.next[head], self.prev[tail] = tail, head
+
+    def _remove(self, key):
+        """ MRU algorithm, remove element """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        """ MRU algorithm, add element """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.prev[self.tail]))
+            self._remove(self.prev[self.tail])
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
 
     def put(self, key, item):
-        """ Function put """
-
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                array_key = list(self.cache_data.keys())
-                print("DISCARD: {}".format(array_key[len(array_key) - 1]))
-                self.cache_data.popitem(last=True)
+        """ Assign to the dictionary """
+        if key and item:
+            if key in self.cache_data:
+                self._remove(key)
+            self._add(key, item)
 
     def get(self, key):
-        """ Function get """
-        if key in self.cache_data and key is not None:
-            self.cache_data.move_to_end(key)
-            return(self.cache_data[key])
-        else:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
+            return value
