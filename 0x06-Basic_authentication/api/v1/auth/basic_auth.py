240c0af071aba6d2ad3fe1b2e1af4d1fb472c73b
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-15 16:42:12 -0500

    adding task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Route module for the basic auth
 """
+import base64
 
 from api.v1.auth.auth import Auth
 
@@ -9,3 +10,16 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ basic authentication class """
     pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization """
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        if authorization_header[0:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
