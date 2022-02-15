ecc252284e0a64b36761200ec493c3253dc1db1b
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-15 15:38:50 -0500

    pycodestyle

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,22 @@ from typing import List, TypeVar
 class Auth:
     """ authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns false """
-        return False
+        """ Validate the paths """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        
+        if path[len(path) - 1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
     
     def authorization_header(self, request=None) -> str:
         """ returns none """
-        return None
+        if request is None:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns none """
