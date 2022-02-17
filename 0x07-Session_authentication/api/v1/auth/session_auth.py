685830ccd908b8b9c09300691974056331420e4a
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-17 11:56:05 -0500

    missing import

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Route module for the session auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -13,7 +14,7 @@ class SessionAuth(Auth):
         """ creates a Session ID for a user_id """
         if user_id is None:
             return None
-        if user_id is not a str:
+        if user_id is not str:
             return None
 
         session_id = str(uuid.uuid4())
