a85afba819e85a46db166bb3d511b7422299c1fb
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-17 14:02:15 -0500

    adding task 3

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -22,7 +22,7 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ returns a User ID based on a Session ID """
-        if session_id is None or session_id is not str:
+        if session_id is None or type(session_id) is not str:
             return None
 
         return self.user_id_by_session_id.get(session_id)
