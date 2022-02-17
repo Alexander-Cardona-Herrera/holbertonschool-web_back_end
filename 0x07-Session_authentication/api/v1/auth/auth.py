2f1a9d2b304b58c782f05da2a663ed2da6a7ad65
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-17 11:14:32 -0500

    adding task 0

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Route module for the auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ authentication class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Validate the paths """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[len(path) - 1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ returns none """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns none """
+        return None
