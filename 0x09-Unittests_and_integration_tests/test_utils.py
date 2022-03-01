dbc93dcef9bfaa9a3c15ecf0b0f7c54b60c85f0d
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-03-01 16:48:56 -0500

    adding task 3

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -3,11 +3,11 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Testing class """
+    """ Testing class for nested map"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -29,7 +29,7 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 class TestGetJson(unittest.TestCase):
-    """ Testing class """
+    """ Testing class for get_json """
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -37,17 +37,39 @@ class TestGetJson(unittest.TestCase):
         ])
     
     def test_get_json(self, url, test_payload):
-        """ test method for nested map """
+        """ test method for get_json """
         class Mocked(Mock):
-            ''' mocked class'''
+            """ mocked class """
 
             def json(self):
-                ''' json method mocked'''
+                """ json method mocked """
                 return test_payload
 
         with patch('requests.get') as MockClass:
             MockClass.return_value = Mocked()
             self.assertEqual(get_json(url), test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """ Testing class for memoize """
+
+    def test_memoize(self):
+        """ test method for nested map """
+        class TestClass:
+            """ mocked class """
+            def a_method(self):
+                """ method mocked """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ method mocked """
+                return self.a_method()
+        
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
 
 if __name__ == '__main__':
     unittest.main()
