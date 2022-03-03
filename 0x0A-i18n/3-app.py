a72b76eb288fc80ab44c47bfd64380da8f35d953
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-03-03 17:28:49 -0500

    adding task 3

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module basic Flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ configuration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ method to call the templates """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ method to set lenguage """
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
