892b43f421d5f4614f3d5d7de91e9b1cb609b53e
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-03-03 17:43:21 -0500

    refactoring

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -2,8 +2,7 @@
 """
 Module basic Flask app
 """
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 
@@ -12,7 +11,7 @@ babel = Babel(app)
 
 
 class Config():
-    """ configuration class """
+    """Class Config"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -21,16 +20,17 @@ class Config():
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """ method to call the templates """
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """Index page"""
     return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ method to set lenguage """
-    return request.accept_languages.best_match(["en", "fr"])
+    """Method that determines the best match with
+    our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
