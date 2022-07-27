f78c02039b3d8e74abb28cf6e14c74d1fd5df02a
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-07-26 20:50:51 -0500

    debbing

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""Module task 12
+"""
 from pymongo import MongoClient
 
 
-def helper(a: dict) -> int:
-    """return log"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    return logs.count_documents(a)
-
-
-def main():
-    """ provides some stats about Nginx logs stored in MongoDB """
-    print(f"{helper({})} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {helper({'method': 'GET'})}")
-    print(f"\tmethod POST: {helper({'method': 'POST'})}")
-    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
-    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
-
-
 if __name__ == "__main__":
-    main()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    count_docs = nginx_collection.count_documents({})
+    count_get = nginx_collection.count_documents({'method': 'GET'})
+    count_post = nginx_collection.count_documents({'method': 'POST'})
+    count_put = nginx_collection.count_documents({'method': 'PUT'})
+    count_patch = nginx_collection.count_documents({'method': 'PATCH'})
+    count_del = nginx_collection.count_documents({'method': 'DELETE'})
+    count_path = nginx_collection.count_documents({'path': '/status'})
+
+    dict_methods = {'GET': count_get,
+                    'POST': count_post,
+                    'PUT': count_put,
+                    'PATCH': count_patch,
+                    'DELETE': count_del
+                    }
+
+    print('{} logs\nMethods:'.format(count_docs))
+    for key, value in dict_methods.items():
+        print('\tmethod {}: {}'.format(key, value))
+    print('{} status check'.format(count_path))
