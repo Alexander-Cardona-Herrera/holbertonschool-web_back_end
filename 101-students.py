cb74140be7afa93a0a745fde93b7a0d6fd093acb
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-07-26 21:01:32 -0500

    tryng something

--- 101-students.py
+++ 101-students.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""Module task 14
-"""
-from operator import itemgetter
+""" Top students """
 
 
 def top_students(mongo_collection):
-    """Function that returns all students sorted by average score
-    """
-
-    list_docs = []
-    for doc in mongo_collection.find({}):
-        count = 0
-        for item in doc['topics']:
-            count += item['score']
-        doc['averageScore'] = count / len(doc['topics'])
-        list_docs.append(doc)
-
-    output = sorted(list_docs, key=itemgetter('averageScore'), reverse=True)
-    return output
+    """ returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
